06f726780db2ac20b3cc732af52b4f69a67b648e
Humanshu <humanshujaglan@gmail.com>
2023-10-29 20:52:43 +0530

    main page mostly completed

---
+++ assets/js/marquee.js
@@ -0,0 +1,7 @@
+$(function() {
+    $('marquee').mouseover(function() {
+        $(this).attr('scrollamount',0);
+    }).mouseout(function() {
+         $(this).attr('scrollamount',4);
+    });
+});
