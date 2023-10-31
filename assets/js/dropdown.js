a5820ab960c084e62a9ecb48fdc03341fbc3d4ad
Humanshu <humanshujaglan@gmail.com>
2023-10-31 09:03:35 +0530

    responsivenes for tablet done

---
+++ assets/js/dropdown.js
@@ -0,0 +1,37 @@
+// for dropdowns subdropdowns and sidebar
+document.addEventListener("DOMContentLoaded", function () {
+    const toggleButtonShow = document.getElementById("toggle-show");
+    const toggleButtonCross = document.getElementById("toggle-cross");
+    const toggleMenuButtons = document.querySelectorAll(".toggle-menu");
+    const toggleSubMenuButtons = document.querySelectorAll(".toggle-sub-menu");
+    const dropdowns = document.querySelectorAll(".side-dropdown-content");
+    const subDropdowns = document.querySelectorAll(".side-sub-dropdown-content");
+   
+    const documentationBox = document.querySelector(".sidebar");
+
+    toggleButtonShow.addEventListener("click", function () {
+        documentationBox.classList.toggle("hidden");
+    });
+    toggleButtonCross.addEventListener("click", function() {
+        documentationBox.classList.toggle("hidden");
+    });
+    
+    toggleMenuButtons.forEach((toggleMenuButton, index) => {
+        toggleMenuButton.addEventListener("click", function() {
+            if (dropdowns[index]) {
+                dropdowns[index].classList.toggle("hidden");
+            }
+        });
+    });
+    
+    toggleSubMenuButtons.forEach((toggleSubMenuButton, index) => {
+        toggleSubMenuButton.addEventListener("click", function() {
+            if (subDropdowns[index]) {
+                subDropdowns[index].classList.toggle("hidden");
+            }
+        });
+    });
+});
+
+// Path: assets/js/scroll.js
+
