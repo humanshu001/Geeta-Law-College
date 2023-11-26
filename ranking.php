5461690c70cf3b64804f58ae87eae1312873fbe6
khushi <kushirana639642@gmail.com>
2023-11-26 12:44:43 +0530

    achievement section and our placement program section is completed

---
+++ ranking.php
@@ -0,0 +1,24 @@
+<?php include 'header.php'; ?>
+<div class="conteiner">
+    <div class="chairman">
+        <div class="chairman-text">
+            <h1>RANKINGS</h1>
+            <hr class="lines">
+                    <hr class="orange-line">
+
+                    <ul>
+                        <li>CSR LAW SCHOOOL SURVEY 2023 RANKED “Geeta Institute of Law” as Ranked 4th in Top Outstanding Law School of <br>Excellence.  </li>
+                        <li>Outlook India Ranked “Geeta Institute of Law” as 27th Best Law College amongst top Law Colleges in India in June 2021.</li>
+                        <li>India Today Ranked Geeta Institute of Law 36th among Best Law Colleges in India (India Today – Special Issue – Best <br>Law Colleges in India June 2020.</li>
+                        <li>India Today Ranked Geeta Institute of Law 35th among Best Law Colleges in India (India Today – Special Issue – Best <br>Law Colleges in India June 2019.</li>
+                        <li>CSR-GHRDC Ranked “Geeta Institute of Law” as Ranked 8th in Law School Survey, 2022.</li>
+                        <li>BW LEGAL WORLD Ranked “Geeta Institute of Law” under top 15 private law colleges in North India June 2022.</li>
+                        <li>CSR-GHRDC Ranked “Geeta Institute of Law” as Ranked 11th in Law School Survey, 2021.</li>
+                        <li> CSR-GHRDC Ranked “Geeta Institute of Law” as Ranked 9th in  Law School Survey, 2020.</li>
+                    </ul>
+                    </ul>
+                    </ul>
+        </div>
+    </div>
+</div>
+<?php include 'footer.php'; ?>
