67c8c44e1a1f7520c6795d8100096022d1dda34c
Humanshu <humanshujaglan@gmail.com>
2023-11-26 13:51:31 +0530

    merged

--- advanced_about.php
+++ advanced_about.php
@@ -1,33 +1,39 @@
-<?php include("header.php")?>
+<?php include("header.php") ?>
 
 <div class="container">
-            <div class="chairman">
-                <div class="chairman-text">
-                    <h1>About Us</h1>
-                    <hr class="lines">
-                    <hr class="orange-line"><br>
-<img class="abspace" src="assets/images/advance1.png" alt="">
-<img class="shifted" src="assets/images/advance2.jpg" alt="" width="900px">
-<h3>Vision</h3>
-<p>The Centre for Intellectual Property Research & Advanced Studies (CIPRAS) has been established with the vision to create a platform for creation, protection, analysis, and education in the field of Intellectual Property Rights by organizing various events of Guest Lectures, Certificate Courses, Workshops, Conferences, Training Programmes and Seminars in collaboration with renowned National/International Speakers and Organizations dealings with Intellectual Property. </p><br>
-<h3>Brochure:</h3>
-<p>For More Information kindly go through the given link:</p>
-<a class="aboutcentre" href="assets/files/advances.pdf" target="blank"><p class="aboutcentre">Centre For Intellectual Property Research and Advanced Studies</p></a>
-<br>
-<h3>Faculty Associates</h3><br>
-Dr. Nisha, Assistant Director- CIPRAS <br><br>
+    <div class="chairman">
+        <div class="chairman-text">
+            <h1>About Us</h1>
+            <hr class="lines">
+            <hr class="orange-line"><br>
+            <img class="abspace" src="assets/images/advance1.png" alt="">
+            <img class="shifted" src="assets/images/advance2.jpg" alt="" width="900px">
+            <h3>Vision</h3>
+            <p>The Centre for Intellectual Property Research & Advanced Studies (CIPRAS) has been established with the
+                vision to create a platform for creation, protection, analysis, and education in the field of
+                Intellectual Property Rights by organizing various events of Guest Lectures, Certificate Courses,
+                Workshops, Conferences, Training Programmes and Seminars in collaboration with renowned
+                National/International Speakers and Organizations dealings with Intellectual Property. </p><br>
+            <h3>Brochure:</h3>
+            <p>For More Information kindly go through the given link:</p>
+            <a class="aboutcentre" href="assets/files/advances.pdf" target="blank">
+                <p class="aboutcentre">Centre For Intellectual Property Research and Advanced Studies</p>
+            </a>
+            <br>
+            <h3>Faculty Associates</h3><br>
+            Dr. Nisha, Assistant Director- CIPRAS <br><br>
 
-<span style="font-weight:400">Email:</span>
-<a href="mailto:ccjrgil@geeta.edu.in"> ciprasgil@geeta.edu.in</a><br>
+            <span style="font-weight:400">Email:</span>
+            <a href="mailto:ccjrgil@geeta.edu.in"> ciprasgil@geeta.edu.in</a><br>
 
 
 
 
 
 
-                    
-                </div>
-            </div>
+
         </div>
+    </div>
+</div>
 
-<?php include("footer.php")?>
+<?php include("footer.php") ?>
