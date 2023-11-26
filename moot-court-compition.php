2bdc64e1092d3b16be5a34b06fcb3e3101935df0
diyagupta001 <rekhagupta39762@gmail.com>
2023-11-26 12:49:58 +0530

    syllabus, clubs and study materials section is completed

---
+++ moot-court-compition.php
@@ -0,0 +1,51 @@
+<?php include('header.php'); ?>
+
+<div class="container">
+    <div class="chairman">
+        <div class="chairman-text">
+            <h1>Moot Court</h1>
+            <hr class="lines">
+            <hr class="orange-line">
+             <img height="550px" width="1000px" src="assets/images/mc1.jpg" class="responsive">
+             <div class="img-mcc">
+                <div class="img-mcca">
+                    <img height="400px" width="480px" src="assets/images/mc2.jpg" class="responsive">
+                </div>
+                <div class="img-mccb">
+                    <img height="400px" width="480px" src="assets/images/mc3.jpg" class="responsive">
+                </div>
+             </div>
+             <div class="img-mcc">
+                <div class="img-mcca">
+                    <img height="400px" width="48%" src="assets/images/mc4.jpg" class="responsive">
+                </div>
+                <div class="img-mccb">
+                    <img height="400px" width="48%" src="assets/images/mc5.jpg" class="responsive">
+                </div>
+             </div>
+            <h1>What is mooting?</h1>
+            <p>Mooting is the oral presentation of a legal issue or problem against an opposing counsel before a judge. It is perhaps the closest experience that a student can have whilst at University.</p>
+            <h1>Why should one get involved in mooting?</h1>
+            <p>Mooting now forms a compulsory part of certain law courses but is still a voluntary student – organised activity. At Geeta Institute of Law, we emphasise participation in Moot Court competitions. Gaining mooting experience can have a positive impact on a student’s future career.</p>
+            <p>Mooting will also help you to build confidence in public speaking, general research and presentation skills, which will be useful in any other field.</p>
+        <h1>Organising a mooting competition</h1>
+        <p>At Geeta Institute of Law, we have a student <a class="BBA" href="moot-court-society.php">Moot Court committee</a> and our students participate in all major National and International Moot Court competitions.</p>
+        <p>At Geeta Institute of Law, we organize National level Moot Court competitions to give a platform to our budding lawyers.</p>
+        <h1>Our past Moot court competitions:-</h1>
+        <p>1st National Moot court Competition</p>
+        <p>2nd National Moot court Competition</p>
+        <p>3rd National Moot court Competition</p>
+        <p>4th National Moot court Competition</p>
+        <p>5th National Moot court Competition</p>
+        <p>6th National Moot court Competition</p>
+        <p>7th National Moot court Competition</p>
+        <p>8th National Moot court Competition</p>
+        <p>9th National Moot court Competition</p>
+        <a href="justice1-deepak-national.php" class="BBA"  >1(10) Justice Dipak Misra National Moot Court Competition of 2020</a><br><br>
+        <a href="justice2-deepak-national.php" class="BBA" >2(11) Justice Dipak Misra National Moot Court Competition of 2021</a><br><br>
+        <a href="justice3-deepak-national.php" class="BBA">3(12) Justice Dipak Misra National Moot Court Competition of 2023</a><br><br>
+        <a href="13th-National-Moot-court-Competition.php" class="BBA" >13th National Moot court Competition</a>
+            </div>
+    </div>
+</div>
+<?php include('footer.php'); ?>
