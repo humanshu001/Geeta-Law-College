d98eac7fc4e0c5ca951d0595cccd16080b37eb85
AnkurJaglan07 <ankurjaglan777@gmail.com>
2023-11-28 21:11:44 +0530

    added faqs.php, reception.php, library.php, hostel.php, wifi.php done by Gourav

---
+++ faqs.php
@@ -0,0 +1,33 @@
+<?php include 'header.php'; ?>
+<div class="chairman">
+    <div class="chairman-text">
+    <h1>FAQs</h1>
+        <hr class="lines"><hr class="orange-line">
+        <br>
+        <ol type="1">
+            <li> Which stream should a candidate choose in 12th class to pursue a career in law?</li>
+            <p>A candidate can pursue degree in Law after doing 12th class from any stream i.e. Humanities, Commerce, Medical, Non-Medical etc.</p>
+            <li> Who can pursue LLB?</li>
+            <p>Anyone having a Graduation or Post Graduation degree in any field can pursue LLB.</p>
+            <li> What is the minimum percentage of marks required to take admission in BA.LL.B/BBA.LL.B /LL.B/LL.M?</li>
+            <p>For admission in BA.LL.B/BBA.LL.B a student should have 45% or higher marks in 12th Class subject to the concerned University or Institution and in LL.B a student should have minimum 45% or higher marks in Graduation or Post Graduation subject to the concerned University.</p>
+            <li> What is the age criterion to take admission in BA.LL.B/BBA.LL.B/LL.B/LL.M?</li>
+            <p>The age criterion for admission in BA.LL.B/BBA.LL.B/LLB/LLM is subject to the discretion of concerned University or Institution.</p>
+            <li>Can a person practice in any Legal Field after law or they need to do specialization?</li>
+            <p>A student can practice in any field after his/her Law Degree. There is no need of any specialization but it can be done by the candidate for better practice.</p>
+            <li>Is it possible to do LLB/BA.LL.B/BBA.LL.B in online or distant mode?</li>
+            <p>Being a professional course, LLB/BA.LL.B/BBA.LL.B cannot be done in Online or Distant mode.</p>
+            <li>Who can apply for government jobs after doing Law?</li>
+            <p>Every Law graduate with the required percentage can appear for the government exams.</p> 
+            <li>What is the difference between BA.LL.B and BBA.LL.B?</li>
+            <p>In BA.LL.B the subjects studied with Law are of humanities field and in BBA.LL.B the subjects studied with Law are of Business Administration.</p>
+            <li>What are the Career options after law?</li>
+            <p>There are numerous options available to a student after completing LLB/BA.LL.B/BBA.LL.B. A student can opt for Government jobs and appear for different exams. A student can go for Individual practice, Corporate, Law Firms, and Job in Banking Institutions etc.</p> 
+            <li>What is to be done after Law Degree to practice in Court?</li>
+            <p>The student has to enroll himself/herself with the State Bar Council and then clear the All India Bar Examination and get their License to practice.</p>
+        </ol>
+
+
+        </div>
+</div>
+<?php include 'footer.php'; ?>
