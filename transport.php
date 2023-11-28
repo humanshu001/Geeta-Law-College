43c8f6746fcc77a6077f3d7a5fbf363fe8b2fbf5
AnkurJaglan07 <ankurjaglan777@gmail.com>
2023-11-28 19:09:37 +0530

    added class_room.php , auditorium.php, gymnasium.php , transport.php

---
+++ transport.php
@@ -0,0 +1,97 @@
+<?php include 'header.php'; ?>
+<div class="chairman">
+    <div class="chairman-text">
+    <h1>Auditorium</h1>
+        <hr class="lines"><hr class="orange-line">
+        <img src="assets/images/transport-page.png" width=100% alt="">
+        <p>The students need to commute from different areas of the city to college for which the College is providing Air conditioned deluxe buses ply from Panipat, Karnal, Sonipat, Kurukshetra, Delhi and all nearby areas.</p>
+        <h1><b>ANNUAL TRANSPORT CHARGES</b></h1>
+
+        <table border="1" cellspacing="0">
+            <tr>
+                <td>Delhi</td>
+                <td>&#8377;48500</td>
+            </tr>
+            <tr>
+                <td>Narela City</td>
+                <td>&#8377;41500</td>
+            </tr>
+            <tr>
+                <td>Kundli/Singhu Border</td>
+                <td>&#8377;41500</td>
+            </tr>
+            <tr>
+                <td>Bahalgarh</td>
+                <td>&#8377;31000</td>
+            </tr>
+            <tr>
+                <td>Murthal</td>
+                <td>&#8377;28000</td>
+            </tr>
+            <tr>
+                <td>Sonipat</td>
+                <td>&#8377;31000</td>
+            </tr>
+            <tr>
+                <td>Gannaur</td>
+                <td>&#8377;25000</td>
+            </tr>
+            <tr>
+                <td>Samalkha</td>
+                <td>&#8377;16500</td>
+            </tr>
+            <tr>
+                <td>Panipat</td>
+                <td>&#8377;26500</td>
+            </tr>
+            <tr>
+                <td>Gharuanda</td>
+                <td>&#8377;26500</td>
+            </tr>
+            <tr>
+                <td>Gohana</td>
+                <td>&#8377;29500</td>
+            </tr>
+            <tr>
+                <td>Karnal</td>
+                <td>&#8377;31500</td>
+            </tr>
+            <tr>
+                <td>Traori</td>
+                <td>&#8377;36000</td>
+            </tr>
+            <tr>
+                <td>Pipli(Kurukshetra)</td>
+                <td>&#8377;41500</td>
+            </tr>
+            <tr>
+                <td>Saffidon</td>
+                <td>&#8377;29500</td>
+            </tr>
+            <tr>
+                <td>Jind</td>
+                <td>&#8377;41500</td>
+            </tr>
+            <tr>
+                <td>Matlauda</td>
+                <td>&#8377;27000</td>
+            </tr>
+            <tr>
+                <td>Israna</td>
+                <td>&#8377;27000</td>
+            </tr>
+            <tr>
+                <td>Dahar</td>
+                <td>&#8377;24500</td>
+            </tr>
+            <tr>
+                <td>Shamli</td>
+                <td>&#8377;32500</td>
+            </tr>
+        </table>
+
+
+    </div>
+</div>
+
+<?php include 'footer.php'; ?>
