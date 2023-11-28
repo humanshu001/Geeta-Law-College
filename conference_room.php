0d788b0c6bff335f445762f702be83e2523df3ee
shubh-0213 <shubhu0213@gmail.com>
2023-11-28 22:55:16 +0530

    conference_room.php

---
+++ conference_room.php
@@ -0,0 +1,18 @@
+<?php
+include("header.php");
+?>
+<section>
+    <div>
+        <h1 class="conf">Conference Room</h1>
+        <hr>
+        <hr class="orange-line">
+    </div>
+    <img src="assets/images/conf.png" class="room" alt="">
+    <h3 class="first"><a href="https://www.geetalawcollege.in/">Geeta Institute of Law</a> is a prestigious educational institution that offers comprehensive legal education and training. Within its campus, the institute houses a well-equipped and modern conference room, designed to facilitate various
+academic and professional activities. The Conference Room at the Geeta Institute of Law provides a professional and conducive environment for hosting conferences,
+seminars, workshops, and meetings related to the legal field. The room is designed to accommodate a significant number of participants, with comfortable seating arrangements and ample space for networking and interactions.</h3>
+<br>
+</section>
+<?php
+include("footer.php");
+?>
--- moot_court_room.php
+++ moot_court_room.php
@@ -12,7 +12,7 @@ include("header.php");
     <h3 class="first">A proper scenario based on a courtroom is created for the students to have a real-life experience. They have to pretend to be lawyers throughout the proceeding. The judges may ask them questions to test their conceptual understanding of laws applicable to the problem. But as the demand for lawyers rose, colleges started prioritizing mooting. Many law schools added “Moot Court” as a subject in their syllabus. The motive behind this was to ensure that students understand the various nuances of the law. Mooting would help them to grasp law in its multiple dimensions and depths. 
     </h3>
     <br>
-    <h3 class="first">We have a Moot Court Hall designed like a courtroom for the purpose of conducting Moot Court Competitions. The Moot Court Hall is also used for conducting and judging competitions at the GIL <a href="National Moot Court Competitions">National Moot Court Competitions</a> “PROFESSIONAL LEGALISM” held annually.
+    <h3 class="first">We have a Moot Court Hall designed like a courtroom for the purpose of conducting Moot Court Competitions. The Moot Court Hall is also used for conducting and judging competitions at the GIL <a href="https://www.geetalawcollege.in/campus-life/moot-courts/">National Moot Court Competitions</a> “PROFESSIONAL LEGALISM” held annually.
     </h3>
     <br>
 </section>
