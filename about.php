683a5a042702170dc8ddd6d7a14329ebc7144579
aditisahoo12 <13aditisahoo@gmail.com>
2023-11-29 23:06:14 +0530

    Cafeteria, About, IT-Lab and List of Journals completed

--- about.php
+++ about.php
@@ -7,10 +7,59 @@
             <hr class="lines">
             <hr class="orange-line">
            
-            <a class="aboutcentre" href="private_college.php" target="about"><p class="aboutcentre"> Best Private Law College in Delhi NCR</p></a>
-            <a class="aboutcentre" href="about_ggi.php" target="about"><p class="aboutcentre"> Geeta Group of Institute</p></a>
-            <a class="aboutcentre" href="geeta_law_college.php" target="about"><p class="aboutcentre">About Geeta Institute of Law</p></a>
-            <a class="aboutcentre" href="vision_mission.php" target="about"><p class="aboutcentre">Vision & Mission</p></a>
+            <img class="shift" src="assets/images/law-image.png" alt="" height="500px">
+            <button style="height: 50px; width: 10%; background-color: orangered; color: white; margin-left: 560px">
+                <a href="https://www.youtube.com/watch?v=Gy095OOZaWw" target="_blank">Campus Tour</a>
+            </button>
+            <p>A law school largely has two functions to perform. The first of them is to train students with the ‘case method of instruction’ so that they may become successful practitioners in the legal profession. Secondly, the research culture at the law schools aids in bringing out the reforms that the law and societal issues require; this is done with the help of scientific and analytical studies of existing laws and also by the comparative method of study. It is the duty of a law institute to emphasize, through research and publication and through the character of its instruction, a unique and strategic mode of learning that not only focuses on professional training but also broadens the basis of legal education.<br><br>
+
+            Geeta Institute of Law has a unique distinction as one of the prestigious Law College in the state of Haryana, offering BA. LL.B (Hons.), 5 Years, BBA. LL.B (Hons.) 5 Years, LL.B (3 years) and LL.M (2 years). The institute is very well located on NH-1, G.T. Road, Panipat, just 50 minutes run from Delhi, the capital of India. The institute was granted affiliation by the Kurukshetra University, Kurukshetra in 2007 and affiliation has been approved by the Bar Council of India. The method of teaching in GIL includes Lecture Discussion, Case Law Analysis, Moot Court Training, Project Assignments, Court Visits, Legal Aid Programs and Internship and Placement Programs. The institute organizes seminars on contemporary legal issues and global concern on national and international level, conduct clinical courses and train students in legal research and legal writing. By the time a student completes the program he/she will be fully equipped with the required theoretical knowledge and practical experience in the field of law to become a full-fledged responsible member of the legal profession.</p>
+
+            <h1>Vision & Mission</h1>
+            <hr class="lines">
+            <hr class="orange-line"><br>
+
+            <h3>Vision</h3>
+            
+            <p>To achieve excellence in every field is the broad vision of GIL.</p>
+
+            <ul>
+                <li>Endeavor to place Technocrats on the Global Map as Global Citizens.
+                <li>To impart Futuristic Technical and Professional Education and in still high pattern of Discipline and Sincerity.
+                <li>To foster academic-industry/professional synergies and to forge strategic alliances for greater upward mobility.
+            </ul>
+            <br><br>
+            <h3>Mission</h3>
+            <p>To make incessant endeavor to translate our vision into a reality and achieve the following objectives:</p>
+            <ul>
+                <li>To create world class facilities and ambience for advance level of teaching and practical training.</li>
+                <li>To develop students as global citizens with conscience, commitment and dedication.</li>
+                <li>To enlighten individuals with qualitative and technical parameters and enriching mind skills.</li>
+                <li>To strengthen ties with industries, professional societies, accrediting bodies and statutory authorities.</li>
+                <li>To maintain the No. 1 Institute in Placements thereby ensuring every GIL student gets the best possible placement opportunities.</li>
+            </ul>
+
+            <h1>About GGI</h1>
+            <hr class="lines">
+            <hr class="orange-line">
+
+            <p>Geeta Group of Institutions, run by the K.R. Education Society, was established in historic city of Panipat in 1992. The brand name GGI is well recognized in the field of Education, Engineering, Science & Technology, Management and Law in the region.<br><br>
+
+            Its inception has placed itself at the pioneer Position in the minds and hearts of students. The College was founded with the expressed intention of providing modern technical education to the students. Though originally meant for the students from the state of Haryana, the college today attracts students from all across the country and neighbouring countries due to its popularity.<br><br>
+
+            Geeta Group of Institutions, with strength of more than 7000 students with galaxy of eleven Institutions has been imparting quality education, promises to become a triumph in the field of education for young minds. We would like to invite all the prospective scholars to visit our campus and witness the range of facilities available in the field of Law & Education, Engineering, Management, and Hotel Management for achieving excellence in the relevant area of studies.<p>
+
+            <h1>About Geeta Law College</h1>
+            <hr class="lines">
+            <hr class="orange-line">
+
+            <p>Geeta Institute of Law strongly emerged as one of the top private law colleges in Haryana, Delhi NCR offering BA. LL.B (Hons.) 5 Years,  BBA. LL.B (Hons.) 5 Years, LL.B (3 years) and LL.M (2 years). The institute is located on NH-1 G.T. Road, Panipat, Haryana, India. Just 50 minutes drive away from Delhi.<br><br>
+
+            The institute was established by the visionary and a social leader Sh. S.P. Bansal, under the aegis of K.R. Education Society to achieve world-class legal education in the country. His vision for the Law Institute is to provide excellence in Legal Education and to produce quality law Graduates with good moral principles and great human values. The institute was granted affiliation by Kurukshetra University Kurukshetra in 2007 and the affiliation has been approved by the Bar Council of India.<br><br>
+
+            The methods of teaching in the GIL include lecture discussion, case law analysis, moot court training project assignment and placement programs. The Institute organizes seminars on contemporary legal issues, conducts clinical courses and trains students in legal research and legal writing. By the time a student completes the Program he/she hones in the required theoretical knowledge and practical experience in the field of law to become a full-fledged responsible member of the legal profession.</p>
+
+    
 
         </div>
     </div>
---
+++ cafeteria.php
@@ -0,0 +1,16 @@
+<?php include("header.php")?>
+
+<div class="container">
+    <div class="chairman">
+        <div class="chairman-text">
+            <h1>Cafeteria</h1>
+            <hr class="lines">
+            <hr class="orange-line">
+
+            <img src="assets/images/cafeteria.png" alt="" height="550px" width="1000px"><br>
+            <p>The Cafeteria is the best loved place in the college & is the right place to take a breather between the lectures. It serves food that gets the taste buds tingling for more. It offers hygienically prepared delectable snacks, soft drinks and other assorted items catching the imagination of the young at heart.</p>
+        </div>
+    </div>
+</div>
+
+<?php include("footer.php")?>
