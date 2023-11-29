0ed19b2c77cb3a70426756a6958e323a2d3c090f
Shubh-0213 <2301301027.shubhdeep@geetauniversity.edu.in>
2023-11-29 18:21:27 +0530

    course_structure.php

--- course_structure.php
+++ course_structure.php
@@ -4,7 +4,450 @@ include ('header.php');
 ?>
 
 <section>
-
+    <div class="chairman">
+        <div class="chairman-text">
+            <h1>Course Structure</h1>
+            <hr class="lines">
+            <hr class="orange-line">
+   
+    <details>
+        <summary>BALL.B/BBA LL.B(5 Years)</summary>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester I</td>
+            </tr>
+            <tr>
+                <th>BALL.B(5 Years)</th>
+                <th>BBA LL.B(5 Years)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>General English -I (Grammar and Usage, vocabulary and writing skills)</p></li>
+                        <li><p>Political Science - I</p></li>
+                        <li><p>Sociology - I</p></li>
+                        <li><p>Application of computer in Law</p></li>
+                        <li><p>Law of Torts (including Motor Vehicle Act 1988)</p></li>
+                        <li><p>Law of Contract - I</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>General English -I (Grammar and Usage, vocabulary and writing skills)</p></li>
+                        <li><p>Principles of Management</p></li>
+                        <li><p>Business Communication</p></li>
+                        <li><p>Application of Computer in Law</p></li>
+                        <li><p>Law of Torts (Incl. Motor Vehicle Act 1988)</p></li>
+                        <li><p>Law of Contract-I (General Principles)</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester II</td>
+            </tr>
+            <tr>
+                <th>BALL.B(5 Years)</th>
+                <th>BBA LL.B(5 Years)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>General English -II (Communication Skills, writing skills & introduction to English literature)</p></li>
+                        <li><p>Political Science - II</p></li>
+                        <li><p>Sociology - II</p></li>
+                        <li><p>Legal and Constitutional History</p></li>
+                        <li><p>Law of Consumer Protection Act and Competition</p></li>
+                        <li><p>Law of Contract - II</p></li>
+                        <li><p>Environmental Studies</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>General English -II (Communication Skills, writing skills & introduction to English literature)</p></li>
+                        <li><p>Business Organization</p></li>
+                        <li><p>Managerial Economics</p></li>
+                        <li><p>Law of Contract-II (Special Contracts)</p></li>
+                        <li><p>Environmental Studies</p></li>
+                        <li><p>Legal and Constitutional History</p></li>
+                        <li><p>Law of Consumer Protection and Competition Act,1988</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester III</td>
+            </tr>
+            <tr>
+                <th>BALL.B(5 Years)</th>
+                <th>BBA LL.B(5 Years)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>General English -III (Vocabulary, Advanced writing skills and an introduction to Research Methodology)</p></li>
+                        <li><p>Political Science - III</p></li>
+                        <li><p>Sociology - III</p></li>
+                        <li><p>Constitutional Law of India – I</p></li>
+                        <li><p>Property Law</p></li>
+                        <li><p>Public International Law</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>General English -III (Vocabulary, Advanced writing skills and an introduction to Research Methodology)</p></li>
+                        <li><p>Understanding Human Behavior</p></li>
+                        <li><p>Micro Business Environment</p></li>
+                        <li><p>Constitutional Law of India – I</p></li>
+                        <li><p>Property Law</p></li>
+                        <li><p>Public International Law</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester IV</td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>Economics -I</p></li>
+                        <li><p>Political Science – IV</p></li>
+                        <li><p>Law of Crimes – I (IPC)</p></li>
+                        <li><p>Constitutional Law of India – II</p></li>
+                        <li><p>Jurisprudence</p></li>
+                        <li><p>Environmental Law</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Principles of Banking</p></li>
+                        <li><p>Macro Business Environment</p></li>
+                        <li><p>Law of Crimes – I (IPC)</p></li>
+                        <li><p>Constitutional Law of India – II</p></li>
+                        <li><p>Jurisprudence</p></li>
+                        <li><p>Environmental Law</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester V</td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>Economics -II</p></li>
+                        <li><p>Political Science – V</p></li>
+                        <li><p>Family Law – I</p></li>
+                        <li><p>Law of Crimes – II (Cr.PC)</p></li>
+                        <li><p>Labour and Industrial Law – I</p></li>
+                        <li><p>Company Law & Corporate Governance</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Economics -II</p></li>
+                        <li><p>Principals of Insurance</p></li>
+                        <li><p>Family Law – I</p></li>
+                        <li><p>Law of Crimes – II (Cr.PC)</p></li>
+                        <li><p>Labour and Industrial Law – I</p></li>
+                        <li><p>Company Law & Corporate Governance</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester VI</td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>Economics -III</p></li>
+                        <li><p>Political Science – VI</p></li>
+                        <li><p>Family Law – II</p></li>
+                        <li><p>Law of Evidence</p></li>
+                        <li><p>Labour & Industrial Law – II</p></li>
+                        <li><p>Administrative Law & Right to Information</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Economics – III</p></li>
+                        <li><p>Enterpreneurship Development</p></li>
+                        <li><p>Family Law – II</p></li>
+                        <li><p>Law of Evidence</p></li>
+                        <li><p>Labour & Industrial Law – II</p></li>
+                        <li><p>Administrative Law & Right to Information</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester VII </td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>History – I</p></li>
+                        <li><p>Civil Procedure Code - I</p></li>
+                        <li><p>Principle of Taxation Law</p></li>
+                        <li><p>Interpretation of Statues & Principle of Legislation</p></li>
+                        <li><p>Human Rights Law and Practices / International Trade Law</p></li>
+                        <li><p>Professional Ethics, Accountancy for Lawyers for Bench Bar Relations</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Marketing Management</p></li>
+                        <li><p>Civil Procedure Code - I</p></li>
+                        <li><p>Principle of Taxation Law</p></li>
+                        <li><p>Interpretation of Statues & Principle of Legislation</p></li>
+                        <li><p>Human Rights Law and Practices / International Trade Law</p></li>
+                        <li><p>Professional Ethics, Accountancy for Lawyers for Bench Bar Relations</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester VIII </td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>History – II</p></li>
+                        <li><p>Civil Procedure Code – II and Limitation Act</p></li>
+                        <li><p>Indirect Taxation Laws</p></li>
+                        <li><p>Intellectual Property Law / Investment and Securities Laws</p></li>
+                        <li><p>Gender Justice & Feminist Jurisprudence / Media & Law</p></li>
+                        <li><p>Alternative Dispute Resolution</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Human Resource Management</p></li>
+                        <li><p>Civil Procedure Code – II and Limitation Act</p></li>
+                        <li><p>Indirect Taxation Laws</p></li>
+                        <li><p>Intellectual Property Law / Investment and Securities Laws</p></li>
+                        <li><p>Gender Justice & Feminist Jurisprudence / Media & Law</p></li>
+                        <li><p>Alternative Dispute Resolution</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester IX </td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>Land Laws</p></li>
+                        <li><p>Law relating to Equity, Trust and Specific Relief</p></li>
+                        <li><p>Banking Law (including Negotiable Instrument Act) / Biological Diversity Law</p></li>
+                        <li><p>Criminology, Penology and Victimology / International Criminal Law</p></li>
+                        <li><p>Law relating to Local Self Government and Panchayat Administration / Citizenship and Immigration Law</p></li>
+                        <li><p>Drafting, Pleading and Conveyancing</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Land Laws</p></li>
+                        <li><p>Law relating to Equity, Trust and Specific Relief</p></li>
+                        <li><p>Banking Law (including Negotiable Instrument Act) / Biological Diversity Law</p></li>
+                        <li><p>Criminology, Penology and Victimology / International Criminal Law</p></li>
+                        <li><p>Law relating to Local Self Government and Panchayat Administration / Citizenship and Immigration Law</p></li>
+                        <li><p>Drafting, Pleading and Conveyancing</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+        <table class="course-structure">
+            <tr>
+                <td colspan="2">Semester X </td>
+            </tr>
+            <tr>
+                <th>BALL.B(Hons.)</th>
+                <th>BBA LL.B(Hons.)</th>
+            </tr>
+            <tr>
+                <td>
+                    <ul>
+                        <li><p>Hindi / Urdu / Legal Language and General English</p></li>
+                        <li><p>Law of Insurance / Law relating to Bankruptcy and Insolvency</p></li>
+                        <li><p>Conflict of Law / International Organisations</p></li>
+                        <li><p>Cyber Law / Air and Space Law</p></li>
+                        <li><p>Law relating to Juvenile Justice and Probation of Offenders / Forensic Law</p></li>
+                        <li><p>Moot Court & Internship</p></li>
+                    </ul>
+                </td>
+                <td>
+                    <ul>
+                        <li><p>Hindi / Urdu / Legal Language and General English</p></li>
+                        <li><p>Law of Insurance / Law relating to Bankruptcy and Insolvency</p></li>
+                        <li><p>Conflict of Law / International Organisations</p></li>
+                        <li><p>Cyber Law / Air and Space Law</p></li>
+                        <li><p>Law relating to Juvenile Justice and Probation of Offenders / Forensic Law</p></li>
+                        <li><p>Moot Court & Internship</p></li>
+                    </ul>
+                </td>
+            </tr>
+        </table>
+    </details>
+    <details>
+        <summary>LLB(Three-Years)</summary>
+        <h1>Semester I</h1>
+        <br>
+        <ul class="course-struc">
+            <li>Jurisprudence</li>
+            <li>Constitutional Law -I</li>
+            <li>Family Law- I</li>
+            <li>Law of Crimes-I (IPC)</li>
+            <li>Law of Contract-I</li>
+        </ul>
+        <h1>Semester II</h1>
+        <br>
+        <ul class="course-struc">
+            <li>Public International Law</li>
+            <li>Constitutional Law -II</li>
+            <li>Family Law- II</li>
+            <li>Law of Crimes-II (IPC)</li>
+            <li>Law of Contract-II</li>
+        </ul>
+        <h1>Semester III</h1>
+        <br>
+        <ul class="course-struc">
+            <li>Law of Torts (including Motor Vehicle Act 1988 & Consumer Protection Laws)</li>
+            <li>Law of Evidence</li>
+            <li>Criminology & Victimology</li>
+            <li>Company Law</li>
+            <li>Compulsory Clinical Course: Drafting, Pleading and Conveyancing</li>
+        </ul>
+        <h1>Semester IV</h1>
+        <br>
+        <ul class="course-struc">
+            <li>Property Law</li>
+            <li>Administrative Law and Right to Information</li>
+            <li>Insurance Law</li>
+            <li>Environmental Law</li>
+            <li>Compulsory Clinical Course: Professional Ethics</li>
+        </ul>
+        <h1>Semester V</h1>
+        <br>
+        <ul class="course-struc">
+            <li>Civil Procedure Code & Limitation Act</li>
+            <li>Labour & Industrial Law – I</li>
+            <li>Land Laws</li>
+            <li>Cyber Law / Human Rights & Law of Disability</li>
+            <li>Compulsory Clinical Course: Mediation, Conciliation & Arbitration</li>
+        </ul>
+        <h1>Semester VI</h1>
+        <br>
+        <ul class="course-struc">
+            <li>Principals of Taxation Law</li>
+            <li>Labour & Industrial Law – II</li>
+            <li>Intellectual Property Law / Women & Law</li>
+            <li>Interpretation of Statues</li>
+            <li>Compulsory Clinical Course: Moot Court & Internship</li>
+        </ul>
+    </details>
+    <details>
+        <summary>LLM(Two Year)</summary>
+        <p>Student can choose any of the following groups for specialization in addition to compulsory subjects.</p>
+        <h1>Semester I</h1>
+        <br>
+        Core Papers
+        <ul class="law-course">
+            <li>Law, social Transformation and Judicial Process in India</li>
+            <li>Indian Constitutional Law and New Challenges</li>
+            <li>Indian Constitutional Law and New Challenges</li>
+        </ul>
+        <h1>Semester II</h1>
+        <br>
+        Core Papers
+        <ul class="law-course">
+            <li>Legal Theory</li>
+        </ul>
+        <br>
+        Group C - Business Law
+        <ul class="law-course">
+            <li>Corporate Law & Management</li>
+            <li>Law of Contractual Transactions</li>
+        </ul>
+        <br>
+        Group G- Constitution Law
+        <ul class="law-course">
+            <li>Mass Media and Constitutional Democracy</li>
+            <li>Public Utilities and Welfare State</li>
+        </ul>
+        <h1>Semester III</h1>
+        <br>
+        Core Papers
+        <ul class="law-course">
+            <li>Research Methodology</li>
+        </ul>           
+       <br>
+       Group C - Business Law
+       <ul class="law-course">
+            <li>Law of Banking & Negotiable Instruments</li>
+            <li>Law of Industrial & Intellectual Property</li>
+       </ul>
+       <br>
+       Group G- Constitution Law
+       <ul class="law-course">
+            <li>Constitutionalism: Power of Judicial Review</li>
+            <li>Federalism: Union & State Relations</li>
+       </ul>
+       <br>
+       <h1>Semester IV</h1>
+       <br>
+       Group C - Business Law
+       <ul class="law-course">
+            <li>Insurance Law</li>
+            <li>Legal Regulation of Economic Enterprises</li>
+            <li>Dissertation</li>
+       </ul>
+       <br>
+       Group G- Constitution Law
+       <ul class="law-course">
+            <li>Human Rights: Constitution of India</li>
+            <li>Constitutional Pluralism: Protection of Special National Interests</li>
+            <li>Dissertation</li>
+       </ul>
+    </details>
+    </div>
+    </div>
 </section>
 
 <?php
