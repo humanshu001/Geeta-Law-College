0fc725e79038b4ad45469f0c42963c33ca13cf8a
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-11-30 05:19:51 +0530

    Fix: All under Academic has been re-verified

--- centre_criminal_justice_reforms.php
+++ centre_criminal_justice_reforms.php
@@ -1,19 +1,20 @@
 <?php include("header.php");?>
 
-<div class="container">
-            <div class="chairman">
-                <div class="chairman-text">
-                    <h1>Centre For Criminal Justice Reforms</h1>
-                    <hr class="lines">
-                    <hr class="orange-line">
-                
-                    <button><a href="justice_about.php">About Us</a></button>
-                    <button><a href="justice_events.php">Events</a></button>
-                    
-                    
-                </div>
+<section class="section26">
+    <div class="container">
+        <div class="chairman">
+            <div class="chairman-text">
+                <h1>Centre For Criminal Justice Reforms</h1>
+                <hr class="lines">
+                <hr class="orange-line">
+
+                <button><a href="justice_about.php">About Us</a></button>
+                <button><a href="justice_events.php">Events</a></button>
+
+
             </div>
         </div>
-    
-<?php include("footer.php");?>
+    </div>
+</section>
 
+<?php include("footer.php");?>
--- centre_leadership.php
+++ centre_leadership.php
@@ -1,19 +1,21 @@
 <?php include("header.php");?>
 
-<div class="container">
-            <div class="chairman">
-                <div class="chairman-text">
-                    <h1>Centre for Leadership</h1>
-                    <hr class="lines">
-                    <hr class="orange-line">
+<section class="section34">
+    <div class="container">
+        <div class="chairman">
+            <div class="chairman-text">
+                <h1>Centre For Leadership</h1>
+                <hr class="lines">
+                <hr class="orange-line">
+            
+                <button><a href="leadership_about.php">About Us</a></button>
+                <button><a href="leadership_event.php">Events</a></button>
                 
-                    <button><a href="leadership_about.php">About Us</a></button>
-                    <button><a href="leadership_event.php">Events</a></button>
-                    
-                    
-                </div>
+                
             </div>
         </div>
+    </div>
+</section>
     
 <?php include("footer.php");?>
 
