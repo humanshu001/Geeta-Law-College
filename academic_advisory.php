ebc14e18a3f284f060f5d06600308c8130b13ebd
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-11-29 13:24:39 +0530

    Addition: distinguished_professor & industry_advisory

--- academic_advisory.php
+++ academic_advisory.php
@@ -6,162 +6,162 @@
             <hr class="lines">
             <hr class="orange-line">
         </div>
-        <div class="academic-advisory">
-            <div class="advisory-profs">
+        <div class="advisory">
+            <div class="profs">
                 <img src="assets/images/acad1.jpg" alt="">
                 <div class="content">
                 <h4>Prof. (Dr.) Sanjeevi Shanthakumar</h4>
                 <p>Vice-Chancellor, National Law University, Gandhinagar</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad2.png" alt="">
                 <div class="content">
                 <h4>Prof. (Dr.) Kamal Jeet Singh</h4>
                 <p>Vice-Chancellor, Madhusudan Law University, Cuttack, Odisha is now a honourable member of the Advisory Board of Geeta Institute of Law, Delhi-NCR</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad3.png" alt="">
                 <div class="content">
                 <h4>Prof. (Dr. ) Nishtha Jaswal</h4>
                 <p>Professor, Department of Laws, Panjab University, Chandigarh</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad4.png" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) Vijaya Kumar</h4>
                     <p>Vice Chancellor, National Law Institute University, Bhopal</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad5.jpg" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) Vijender Kumar</h4>
                     <p>Vice Chancellor, National Law University, Nagpur</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad6.jpg" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) Dilip Ukey</h4>
                     <p>Vice Chancellor, Maharashtra National Law University Mumbai.</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad7.jpg" alt="">
                 <div class="content">
                     <h4>Professor (Dr.) Kesava Rao</h4>
                     <p>Vice-Chancellor, National University of Study and Research in Law, Ranchi.</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad8.jpg" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) Viney Kapoor</h4>
                     <p>Vice Chancellor, Dr. Bhim Rao Ambedkar National Law University, Sonipat</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad11.jpg" alt="">
                 <div class="content">
                     <h4>Dr. Abha Yadav</h4>
                     <p>Director of the Forum of Indian, Regulators (FOIR) Centre at Indian Institute of Corporate Affairs</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad12.jpg" alt="">
                 <div class="content">
                     <h4>Prof. Dr. Pinki Sharma</h4>
                     <p>Professor of Law Law Center-II, University of Delhi</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad13.jpg" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) SC Raina</h4>
                     <p>Vice Chancellor (Former) HP National Law University, Shimla</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad14.jpg" alt="">
                 <div class="content">
                     <h4>Prof. Dr. Mrinal Raste</h4>
                     <p>Vice Chancellor (Former) Symbiosis International University, Pune</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad15.jpg" alt="">
                 <div class="content">
                     <h4>Prof. Dr. S. S. Jaswal</h4>
                     <p>Registrar, HP National Law University, Shimla</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad16.png" alt="">
                 <div class="content">
                     <h4>ADV. MANUJ BHARDWAJ</h4>
                     <p>Executive Secretary (Honorary), INDIAN NATIONAL ASSOCIATION OF LEGAL PROFESSIONALS</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad17.jpg" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) Antoniette Elia</h4>
                     <p>Senior Associate Fellow, University of Santiago de Compostila, Spain</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad18.jpg" alt="">
                 <div class="content">
                     <h4>Dr. Manjula Batra</h4>
                     <p>Professor, Dean (Former) Department of Law, Jamia Milia Islamia University, Delhi</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad19.png" alt="">
                 <div class="content">
                     <h4>Prof. V N Rajasekharan Pillai</h4>
                     <p>Former Chairman, University Grants Commission (UGC), New Delhi Distance Education Council, Govt. of India, New Delhi Former Vice-Chancellor, Indira Gandhi National Open University (IGNOU), New Delhi Mahatma Gandhi University, Kottayam, Kerala Cochin University of Science and Technology</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad20.png" alt="">
                 <div class="content">
                     <h4>Prof. G. Ram</h4>
                     <p>Head, Department of Sociology Director, Centre for Studies in Human Development, Assam University</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad21.png" alt="">
                 <div class="content">
                     <h4>Prof. Dr. Rajpal Sharma</h4>
                     <p>Dean & Chairman, Dept. of Law,Kurukshetra University, Kurukshetra</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad22.png" alt="">
                 <div class="content">
                     <h4>Prof. Manoj Kumar Sinha</h4>
                     <p>Director, The Indian Law Institute New Delhi</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad23.png" alt="">
                 <div class="content">
                     <h4>Prof. (Dr.) Paramjit S. Jaswal,</h4>
                     <p>Vice-Chancellor, Rajiv Gandhi National University of Law, Patiala, Punjab</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad24.png" alt="">
                 <div class="content">
                     <h4>Lt. Gen. Zameer Uddin Shah</h4>
                     <p>Ex-Vice-Chancellor, Aligarh Muslim University, Aligarh</p>
                 </div>
             </div>
-            <div class="advisory-profs">
+            <div class="profs">
                 <img src="assets/images/acad25.png" alt="">
                 <div class="content">
                     <h4>Hon'ble Mr. Justice R.K. Garg (Retd.)</h4>
---
+++ industry_advisory.php
@@ -0,0 +1,182 @@
+<?php include 'header.php'; ?>
+
+<section class="section8">
+    <div class="container">
+        <div class="chairman">
+            <div class="chairman-text">
+                <h1>Industrial Advisory Board</h1>
+                <hr class="lines">
+                <hr class="orange-line">
+                <img src="assets/images/industrial-advisory-board.png" class="industrial-img" alt="">
+                <h2 class="align-left">
+                    ABOUT
+                </h2>
+                <p>
+                    The Industrial Advisory Board of Geeta Institute of Law is a member- based network of
+                    volunteer-leaders/professionals/industrialists that are recognized by society as key leaders in
+                    legal and industry field. <br>
+                    It has been established with a commitment to fill the gaps between the Legal academia which largely
+                    focuses on black-letter law and the practical aspect on the other hand. <br><br>
+                    It is intended to help to initiate close interaction between the institute and the industry and is
+                    serving as a platform for showcasing best practice, latest technological advancements and their
+                    implication. <br><br>
+                    Through, IAB, we expect to improve the quality of legal education adequately and meet the needs of
+                    the industry and academia based on cross-fertilization of ideas for system improvement.
+                </p>
+                <h2 class="align-left">
+                    PURPOSE OF THE BOARD
+                </h2>
+                <ul>
+                    <li>
+                        <p>A few of the primary aims of the board shall be to highlight the curriculum quality.</p>
+                    </li>
+                    <li>
+                        <p>To introduce the contemporary developments in respective legal areas.</p>
+                    </li>
+                    <li>
+                        <p>To emphasize on the holistic development of the students.</p>
+                    </li>
+                    <li>
+                        <p>To create knowledgeable and skilled manpower for meeting current and future demands of
+                            industry, government, research organizations and entrepreneurial pursuits.</p>
+                    </li>
+                    <li>
+                        <p>To create sensitivity to social and professional development.</p>
+                    </li>
+                </ul>
+                <h2 class="align-left">
+                    ROLE OF THE MEMBERS OF THE BOARD
+                </h2>
+                <ul>
+                    <li>
+                        <p>To work to develop innovative ideas/useful content for expanding and propelling legal
+                            Knowledge and skills in various programmes of GIL</p>
+                    </li>
+                    <li>
+                        <p>From time to time, offer valuable career guidance to the pen-ultimate students of GIL.</p>
+                    </li>
+                    <li>
+                        <p>To provide separate mentorship to students interested in Corporate Career.</p>
+                    </li>
+                    <li>
+                        <p>To come with the contemporary developments in practical legal area and will focus to
+                            inculcate them in the teaching pedagogy.</p>
+                    </li>
+                </ul>
+                <h2 class="align-left">
+                    HONORARY MEMBERS OF THE BOARD
+                </h2>
+                <div class="advisory">
+                    <div class="profs">
+                        <img src="assets/images/unknown.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Mahendra Vikram Singh</h4>
+                            <p>Advocate Supreme Court of India,<br>Additional Central Government, Standing Counsel.</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial2.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Arijit Prasad</h4>
+                            <p>Senior Advocate, <br> Supreme Court of India</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial3.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Gauhar Mirza</h4>
+                            <p>Partner, <br> Shardul Amarchand Mangaldas & CO</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial4.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. J.S Bedi</h4>
+                            <p>Managing Partner, <br> J.S Bedi & Associates
+                            </p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial5.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Iram Majid</h4>
+                            <p>Executive Director, <br> Asia Pacific Centre for Arbitration and Meditation </p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial6.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Bharat Sharma</h4>
+                            <p>Managing Partner, <br> ZestIP (LAW FIRM)</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial7.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Sandip Gupta</h4>
+                            <p>Founder & Principal Partner, <br> SG Law Office</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial8.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Manuj Bhardwaj</h4>
+                            <p>Indian National Association of Legal Professionals</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial9.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Pragyan Pradip Sharma</h4>
+                            <p>Legal Counsel, <br> Supreme Court of India</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial10.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Zaryab J Rizvi</h4>
+                            <p>Founding Partner, <br> LCZF (Law Firm)</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial11.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Jaideep Singh</h4>
+                            <p>Managing Partner, <br> Dharmvidhi Legal Services and Solutions LLP</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial12.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Shivinder K Chopra</h4>
+                            <p>Chief Advisor, <br> Legalimus Consultus</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial13.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Sanchari Goswami</h4>
+                            <p>Corporate Counsel, <br> IBUS Network and Infrastructure Pvt. Ltd.</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial14.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Krishan Mohan Bansal</h4>
+                            <p>Managing Partner, <br> Bansal & Associates</p>
+                        </div>
+                    </div>
+                    <div class="profs">
+                        <img src="assets/images/industrial15.jpg" alt="">
+                        <div class="content">
+                            <h4>Adv. Kanika Bhardwaj</h4>
+                            <p>Founder & Partner, <br> Kanika Bhardwaj Law Firm</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<?php include 'footer.php'; ?>
