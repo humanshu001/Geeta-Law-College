d292934f5b8be86d28fb253430310e924556f2fd
aditisahoo12 <13aditisahoo@gmail.com>
2023-11-30 19:46:43 +0530

    Map and Address section is done

---
+++ contact.php
@@ -0,0 +1,105 @@
+<?php include("header.php")?>
+
+<div class="container">
+    <div class="chairman">
+        <div class="chairman-text">
+
+            <div id="map">
+                <iframe loading="lazy"
+                    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3480.282152530653!2d77.00455731506273!3d29.274043661507125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390dc58995555555%3A0xc79d1b29c48477f7!2sGeeta+Institute+of+Law!5e0!3m2!1sen!2sin!4v1450415044432"
+                    width="100%" height="350" frameborder="0" style="border:0" allowfullscreen=""
+                    data-rocket-lazyload="fitvidscompatible"
+                    data-lazy-src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3480.282152530653!2d77.00455731506273!3d29.274043661507125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390dc58995555555%3A0xc79d1b29c48477f7!2sGeeta+Institute+of+Law!5e0!3m2!1sen!2sin!4v1450415044432"
+                    data-ll-status="loaded" class="entered lazyloaded"></iframe>
+                <noscript>
+                    <iframe
+                        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3480.282152530653!2d77.00455731506273!3d29.274043661507125!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390dc58995555555%3A0xc79d1b29c48477f7!2sGeeta+Institute+of+Law!5e0!3m2!1sen!2sin!4v1450415044432"
+                        width="100%" height="350" frameborder="0" style="border:0" allowfullscreen></iframe>
+                </noscript>
+            </div>
+
+            <div class="contact">
+
+                <div class="cont_one">
+                    <h4>Contact Us</h4>
+                    <p>If you have any questions, feedback or other inquiries, contact us and a customer service
+                        representative will gladly assist you.</p>
+
+                    <form>
+
+                        <input type="text" name="names" value="" size="40"
+                            class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required frmfd" aria-required="true"
+                            aria-invalid="false" placeholder="Name*" width=100%>
+                        <br>
+                        <input type="email" name="email" value="" size="40"
+                            class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email email frmfd"
+                            aria-required="true" aria-invalid="false" placeholder="Email Id*"><br>
+
+                        <input type="text" name="mobile-number" value="" size="40"
+                            class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required requiredField frmfd"
+                            aria-required="true" aria-invalid="false" placeholder="Mobile*"><br><br>
+
+                        <input type="text" name="location" value="" size="40"
+                            class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required requiredField frmfd"
+                            aria-required="true" aria-invalid="false" placeholder="Location*"><br><br>
+
+                        <textarea name="message" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea frmfd"
+                            aria-invalid="false" placeholder="Message"></textarea>
+
+                        <div class="controls"> <input type="hidden" name="_wpcf7_captcha_challenge_my-captcha"
+                                value="352474872"><img
+                                class="wpcf7-form-control wpcf7-captchac wpcf7-captcha-my-captcha" width="72"
+                                height="24" alt="captcha"
+                                src="https://www.geetalawcollege.in/wp-content/uploads/wpcf7_captcha/352474872.png">
+                            <span class="wpcf7-form-control-wrap my-captcha"><input type="text" name="my-captcha"
+                                    value="" size="40" class="wpcf7-form-control wpcf7-captchar" autocomplete="off"
+                                    aria-invalid="false"></span> </div>
+
+                        <button type="submit" class="submit-btn">Submit</button>
+                    </form>
+
+                </div>
+                <div class="cont_two">
+                    <h3>Office Address</h3>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                        <path
+                            d="M51.7 295.1l31.7 6.3c7.9 1.6 16-.9 21.7-6.6l15.4-15.4c11.6-11.6 31.1-8.4 38.4 6.2l9.3 18.5c4.8 9.6 14.6 15.7 25.4 15.7c15.2 0 26.1-14.6 21.7-29.2l-6-19.9c-4.6-15.4 6.9-30.9 23-30.9h2.3c13.4 0 25.9-6.7 33.3-17.8l10.7-16.1c5.6-8.5 5.3-19.6-.8-27.7l-16.1-21.5c-10.3-13.7-3.3-33.5 13.4-37.7l17-4.3c7.5-1.9 13.6-7.2 16.5-14.4l16.4-40.9C303.4 52.1 280.2 48 256 48C141.1 48 48 141.1 48 256c0 13.4 1.3 26.5 3.7 39.1zm407.7 4.6c-3-.3-6-.1-9 .8l-15.8 4.4c-6.7 1.9-13.8-.9-17.5-6.7l-2-3.1c-6-9.4-16.4-15.1-27.6-15.1s-21.6 5.7-27.6 15.1l-6.1 9.5c-1.4 2.2-3.4 4.1-5.7 5.3L312 330.1c-18.1 10.1-25.5 32.4-17 51.3l5.5 12.4c8.6 19.2 30.7 28.5 50.5 21.1l2.6-1c10-3.7 21.3-2.2 29.9 4.1l1.5 1.1c37.2-29.5 64.1-71.4 74.4-119.5zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm144.5 92.1c-2.1 8.6 3.1 17.3 11.6 19.4l32 8c8.6 2.1 17.3-3.1 19.4-11.6s-3.1-17.3-11.6-19.4l-32-8c-8.6-2.1-17.3 3.1-19.4 11.6zm92-20c-2.1 8.6 3.1 17.3 11.6 19.4s17.3-3.1 19.4-11.6l8-32c2.1-8.6-3.1-17.3-11.6-19.4s-17.3 3.1-19.4 11.6l-8 32zM343.2 113.7c-7.9-4-17.5-.7-21.5 7.2l-16 32c-4 7.9-.7 17.5 7.2 21.5s17.5 .7 21.5-7.2l16-32c4-7.9 .7-17.5-7.2-21.5z" />
+                    </svg>
+                    <b>Address: </b>Geeta Institute of Law, Opposite DPS Panipat City, GT Road, Panipat, Haryana<br>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                        <path
+                            d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
+                    </svg>
+                    <b>Email: </b>
+                    admissiongil@geeta.edu.in<br>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+                        <path
+                            d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z" />
+                    </svg>
+                    <b>Phone: </b>
+                    9729970000, 9996033743<br><br>
+
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                        <path
+                            d="M51.7 295.1l31.7 6.3c7.9 1.6 16-.9 21.7-6.6l15.4-15.4c11.6-11.6 31.1-8.4 38.4 6.2l9.3 18.5c4.8 9.6 14.6 15.7 25.4 15.7c15.2 0 26.1-14.6 21.7-29.2l-6-19.9c-4.6-15.4 6.9-30.9 23-30.9h2.3c13.4 0 25.9-6.7 33.3-17.8l10.7-16.1c5.6-8.5 5.3-19.6-.8-27.7l-16.1-21.5c-10.3-13.7-3.3-33.5 13.4-37.7l17-4.3c7.5-1.9 13.6-7.2 16.5-14.4l16.4-40.9C303.4 52.1 280.2 48 256 48C141.1 48 48 141.1 48 256c0 13.4 1.3 26.5 3.7 39.1zm407.7 4.6c-3-.3-6-.1-9 .8l-15.8 4.4c-6.7 1.9-13.8-.9-17.5-6.7l-2-3.1c-6-9.4-16.4-15.1-27.6-15.1s-21.6 5.7-27.6 15.1l-6.1 9.5c-1.4 2.2-3.4 4.1-5.7 5.3L312 330.1c-18.1 10.1-25.5 32.4-17 51.3l5.5 12.4c8.6 19.2 30.7 28.5 50.5 21.1l2.6-1c10-3.7 21.3-2.2 29.9 4.1l1.5 1.1c37.2-29.5 64.1-71.4 74.4-119.5zM0 256a256 256 0 1 1 512 0A256 256 0 1 1 0 256zm144.5 92.1c-2.1 8.6 3.1 17.3 11.6 19.4l32 8c8.6 2.1 17.3-3.1 19.4-11.6s-3.1-17.3-11.6-19.4l-32-8c-8.6-2.1-17.3 3.1-19.4 11.6zm92-20c-2.1 8.6 3.1 17.3 11.6 19.4s17.3-3.1 19.4-11.6l8-32c2.1-8.6-3.1-17.3-11.6-19.4s-17.3 3.1-19.4 11.6l-8 32zM343.2 113.7c-7.9-4-17.5-.7-21.5 7.2l-16 32c-4 7.9-.7 17.5 7.2 21.5s17.5 .7 21.5-7.2l16-32c4-7.9 .7-17.5-7.2-21.5z" />
+                    </svg>
+                    <b>Delhi Office Address: </b>
+                    788, 7th Floor, Aggarwal Cyber Plaza - 2, Netaji Subhash Place, Pitampura, Delhi - 110084<br>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="16" viewBox="0 0 512 512">
+                        <path
+                            d="M64 112c-8.8 0-16 7.2-16 16v22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1V128c0-8.8-7.2-16-16-16H64zM48 212.2V384c0 8.8 7.2 16 16 16H448c8.8 0 16-7.2 16-16V212.2L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64H448c35.3 0 64 28.7 64 64V384c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V128z" />
+                    </svg>
+                    <b>Email:</b> admissiongil@geeta.edu.in<br>
+                    <svg xmlns="http://www.w3.org/2000/svg" height="16" width="12" viewBox="0 0 384 512">
+                        <path
+                            d="M16 64C16 28.7 44.7 0 80 0H304c35.3 0 64 28.7 64 64V448c0 35.3-28.7 64-64 64H80c-35.3 0-64-28.7-64-64V64zM224 448a32 32 0 1 0 -64 0 32 32 0 1 0 64 0zM304 64H80V384H304V64z" />
+                    </svg>
+                    <b>Phone: </b>8684891000
+
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include("footer.php")?>
