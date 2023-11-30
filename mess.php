65575690277ce6b806de7ff0776b8f5cf1bd2797
TheKaushikGoswami <thekaushikgoswami@gmail.com>
2023-11-30 22:11:46 +0530

    Fix: mess.php & style.css

--- mess.php
+++ mess.php
@@ -1,6 +1,7 @@
 <?php
 include("header.php");
 ?>
+
 <section class="section54">
     <div class="container">
         <div class="chairman-text">
@@ -12,16 +13,18 @@ include("header.php");
                 dedicated dining area where students can enjoy their meals during their academic tenure. It is
                 designed to cater to the dietary needs and preferences of the students, offering a variety of food
                 options.
-            <br><br>
-            The mess strives to provide a clean and hygienic environment for students to have
-            their meals. It ensures that the food is prepared and served following proper hygiene standards. The
-            menu in the mess usually includes vegetarian dishes, along with options for breakfast, lunch, and
-            dinner. The institute takes into account the diverse cultural and regional backgrounds of its
-            students and tries to incorporate a variety of cuisines into its menu. The mess staff aims to
-            provide nutritious and balanced meals to the students, focusing on their overall well-being.</p>
+                <br><br>
+                The mess strives to provide a clean and hygienic environment for students to have
+                their meals. It ensures that the food is prepared and served following proper hygiene standards. The
+                menu in the mess usually includes vegetarian dishes, along with options for breakfast, lunch, and
+                dinner. The institute takes into account the diverse cultural and regional backgrounds of its
+                students and tries to incorporate a variety of cuisines into its menu. The mess staff aims to
+                provide nutritious and balanced meals to the students, focusing on their overall well-being.
+            </p>
         </div>
     </div>
 </section>
+
 <?php
 include("footer.php");
 ?>
